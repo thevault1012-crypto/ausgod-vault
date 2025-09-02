8926a3e68dc336686d63b81ddfe6765ffd5fb8cb
thevault1012-crypto <thevault1012@gmail.com>
2025-09-02 17:12:24 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./vault-app.js"   // <-- add this line
   ],
   theme: {
     extend: {},
